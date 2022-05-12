851dd2407214ef0c95785d7376a4b8f99c4340d3
Veronika Lebedeva <veronialebedeva@gmail.com>
2022-05-12 14:37:27 +0300

    progweek9

---
+++ WishList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishList = new List<string>();
+
+            Console.WriteLine(myWishList.Count);
+
+            myWishList.Add("coffee machine");
+            myWishList.Add("pair of socks");
+            myWishList.Add("bunjy jump ticket");
+            myWishList.Add("new bed");
+
+            Console.WriteLine(myWishList.Count);
+
+            Console.WriteLine(myWishList[0]);
+            Console.WriteLine(myWishList[1]);
+            Console.WriteLine(myWishList[2]);
+
+            foreach(string wish in myWishList)
+            {
+                Console.WriteLine(wish);
+            }
+        }
+    }
+}
